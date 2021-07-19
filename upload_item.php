09bc93e7fc67f3fd232ad4db7df393315d87e0b4
TeoBoutakoglou <44444802+TeoBoutakoglou@users.noreply.github.com>
2021-07-19 12:56:20 +0300

    Update upload_item.php
    Add
    get the item from home.php
    get the item name
    get the item extension
    check the item size
    check the item extension (format)

--- upload_item.php
+++ upload_item.php
@@ -1,7 +1,24 @@
 <?php
 
-echo "uploading the file....";
+//echo "uploading the file....";
+$target_item = basename($_FILES["itemToUpload"]["name"]);
+echo "Item name-> " . $target_item;
+$itemType = strtolower(pathinfo($target_item,PATHINFO_EXTENSION));
+echo " Extension-> " . $itemType;
 
+
+// Check item size (Bytes)
+if ($_FILES["itemToUpload"]["size"] > 567645 ) {
+    echo "<br>Sorry, your item is too large.";
+}
+
+// Allow certain item formats
+if($itemType != "jpg" && $itemType != "mkv" && $itemType != "pdf") {
+  echo "<br>Sorry, only JPG, MKV & PDF items are allowed.";
+}       
+
+//TODO: να βάλω τα if statement σε δικές τους συναρτήσεις στο function.php    
+//TODO: see the uploading file section on https://www.w3schools.com/php/php_file_upload.asp
 
 
 
