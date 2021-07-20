54ecf54f2e51e005daaa39d08cd942e126002cc9
TeoBoutakoglou <44444802+TeoBoutakoglou@users.noreply.github.com>
2021-07-20 13:19:40 +0300

    test the undo revert (or cherry pick works) and information update for is__file()
    I select the "revert changes in commit" at the last commit an after that do the "cherry-pick commit" if it give the last commmit back. After that push the new changes in upload-items branch and see the result from the web.
    Also the is__file() at the etc/functions.php has 2 underscores due to the php built-in is_file() because php cannot override methods.

--- home.php
+++ home.php
@@ -33,7 +33,8 @@
         }
     }
     
-    if(isset($_SESSION["successfulUploadedItemMessage"])){
+    if(isset($_SESSION["successfulUploadedItemMessage"]))
+    {
         $successfulUploadedItemMessage = $_SESSION["successfulUploadedItemMessage"];
         echo "<br>" . $successfulUploadedItemMessage;
     }
