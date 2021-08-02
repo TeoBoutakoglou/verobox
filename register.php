ae6025161ce89aa70ef8ba1be3510a41e9d3a2f6
TeoBoutakoglou <44444802+TeoBoutakoglou@users.noreply.github.com>
2021-08-02 12:21:55 +0300

    Add styling register.php with css
    Add register avatar (PNG image) for register.php
    
    Add in register.php: Move the head element after the top php code and add to it some meta elements and the link for font awesome icons and the javascript functions script. Also add the register-box which is used as a container for all the components. Add the avatar path and the register-message class. Inside of the form add 2 div elements one for the left side and one for right side which are used as containers for the elements to be placed on the left side and the ride side. The left container conains the elements for first name, last name, username and password and the right container contains the elements for email date of birth and sex of the user. At the end is the register button. The input-error and link for the login page is outside of the form.
    
    Add in style.css: import some fonts from google, add the body element, classes for register-box, register-avatar, register-message, input-box, input[type="text"], .register-box input[type="password"], input[type="submit"], input[type="submit"]:hover, ::placeholder and input-error which is the same as login one's with some minor changes to pair with the left and right components.
    
    Extras: delete unused background for login.php

--- register.php
+++ register.php
@@ -1,11 +1,4 @@
-<!DOCTYPE HTML> 
-<html>
-  <head>
-    <title>Register - Verobox</title>
-    <link rel="stylesheet" type="text/css" href="styles/register/css/style.css">
-  </head>
-
-  <?php
+<?php
     include_once './etc/php/functions.php';
 
     $givenFirstName = $givenLastName = $givenUsername = $givenPassword = $givenEmail = $givenDayOfBirth = $givenMonthOfBirth = $givenYearOfBirth = $givenGender = null;
@@ -73,61 +66,75 @@
       set_toast_message('successfulRegistrationMessage', $givenUsername . ', your account was successfully created, please login to start');
       redirect_to('login.php');
     }
-  
-    
+?>
 
-
-
-  ?>
-
-
+<!DOCTYPE HTML> 
+<html>
+  <head>
+  	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="styles/register/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="./etc/js/functions.js"></script>
+    <title>Register - Verobox</title>
+  </head>
 
 
   <body>
 
+	<div class="register-box">
+		
+		<img src=".\styles\register\images\register-user-avatar-icon.png" class="register-avatar">
+		<!--register page message-->
+		<div class="register-message">Register</div>
 
-    <!--register page message-->
-    <h2>Register</h2>
+		<!--register form-->
+	 	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
 
+          <div class="left-components">
+                <div class="input-box">
+                  <input type="text" id="firstName" name="registerFirstName" placeholder="Enter your first name" value= "<?php echo $givenFirstName?>" autofocus>
+                </div>
 
+                <div class="input-box">
+                  <input type="text" id="lastName" name="registerLastName" placeholder="Enter your last name" value= "<?php echo $givenLastName?>">
+                </div>
 
-    <!--register form-->
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+                <div class="input-box">
+                  <input type="text" id="username" name="registerUsername" placeholder="Enter your username" value= "<?php echo $givenUsername?>" required="username is required">
+                  <span class="input-error">* <?php echo $usernameErr;?></span>
+                </div>
 
-      <label for="lblRegisterFirstName">First name:</label><br>
-      <input type="text" id="firstName" name="registerFirstName" placeholder="Enter your first name" value= "<?php echo $givenFirstName?>" autofocus><br>
-      
-      <label for="lblRegisterLastName">Last name:</label><br>
-      <input type="text" id="lastName" name="registerLastName" placeholder="Enter your last name" value= "<?php echo $givenLastName?>"><br>
+                <div class="input-box">
+                  <input type="password" id="password" name="registerPassword" placeholder="Enter your password"  required="password is required">
+                  <span class="input-error">* <?php echo $passwordErr;?></span>
+                </div>
+          </div>
 
-      <label for="lblRegisterUsername">Username:</label><br>
-      <input type="text" id="username" name="registerUsername" placeholder="Enter your username" value= "<?php echo $givenUsername?>" required="username is required">
-      <span class="error">* <?php echo $usernameErr;?></span><br>
+          <div class="right-components">
+                <div class="input-box">
+                  <input type="text" id="email" name="registerEmail" placeholder="Enter your email" value= "<?php echo $givenEmail?>"  required="email is required">
+                  <span class="input-error">* <?php echo $emailErr;?></span>
+                </div>
 
-      <label for="lblRegisterPassword">Password:</label><br>
-      <input type="password" id="password" name="registerPassword" placeholder="Enter your password"  required="password is required">
-      <span class="error">* <?php echo $passwordErr;?></span><br>
+                <div class="input-box">
+                  <input type="text" id="day" name="dayOfBirth" placeholder="Day" value= "<?php echo $givenDayOfBirth?>">
+                  <input type="text" id="month" name="monthOfBirth" placeholder="Month" value= "<?php echo $givenMonthOfBirth?>">
+                  <input type="text" id="year" name="yearOfBirth" placeholder="Year" value= "<?php echo $givenYearOfBirth?>">
+                </div>
 
-      <label for="lblRegisterEmail">E-mail:</label><br>
-      <input type="text" id="email" name="registerEmail" placeholder="Enter your email" value= "<?php echo $givenEmail?>"  required="email is required">
-      <span class="error">* <?php echo $emailErr;?></span><br>
+                
+                  <input type="radio" id="maleGender" name="gender" value="Male" <?php if ($givenGender == 'Male') { echo 'checked'; } ?>>
+                  <label for="maleGender">Male</label>
+                  <input type="radio" id="femaleGender" name="gender" value="Female" <?php if ($givenGender == 'Female') { echo 'checked'; } ?>>
+                  <label for="femaleGender">Female</label>
+          </div>
+			    
+          <input type="submit" value="Register">
+		</form>
 
-      <label for="dateSelectRegisterDateOfBirth">Date of birth:</label><br>
-      <input type="text" id="day" name="dayOfBirth" placeholder="Day" value= "<?php echo $givenDayOfBirth?>">
-      <input type="text" id="month" name="monthOfBirth" placeholder="Month" value= "<?php echo $givenMonthOfBirth?>">
-      <input type="text" id="year" name="yearOfBirth" placeholder="Year" value= "<?php echo $givenYearOfBirth?>"><br>
-
-      <label for="radioButtonRegisterGender">Gender:</label><br>
-      <input type="radio" id="maleGender" name="gender" value="Male" <?php if ($givenGender == 'Male') { echo 'checked'; } ?>>
-      <label for="maleGender">Male</label>
-      <input type="radio" id="femaleGender" name="gender" value="Female" <?php if ($givenGender == 'Female') { echo 'checked'; } ?>>
-      <label for="femaleGender">Female</label><br>
-
-      <input type="submit" value="Register">
-    </form>
-
-    <br><span class="error">* field is Required</span><br>
-    <br><a href='./login.php'>Έχεις ήδη λογαριασμό?</a>
-
+		<span class="input-error">* field is Required</span><br>
+		<a href='./login.php'>Έχεις ήδη λογαριασμό?</a>
+	</div>
   </body>
 </html>
